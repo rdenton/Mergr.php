63df7a5c2da01da904a436e293f8a1bb011161cc
emarketeer <info@emarketeer.com.au>
2013-09-12 14:01:33 +1000

    Added --watch and --no-plugins

--- lib/mergr.class.php
+++ lib/mergr.class.php
@@ -2,7 +2,9 @@
 
 class mergr {
 	
-	public function __construct($directory=null) {
+	public function __construct($directory=null,$options) {
+		
+		$this->options = $options;
 		
 		//Make sure we have a directory to work with.
 		if (! $directory) {
@@ -50,7 +52,7 @@ class mergr {
 				if ( file_exists($directory . "/" . $file) && !is_dir($directory . "/" . $file) ) {
 					//Valid file.
 					//Check for CSS extension.
-					if ( substr($file, -strlen($file_extension)) == $file_extension && $file != "global.min." . $file_extension) {
+					if ( substr($file, -strlen($file_extension)) == $file_extension && $file != "global.min" . $file_extension) {
 						$return_array[] = $directory . "/" . $file;
 					}
 				}
@@ -99,8 +101,10 @@ class mergr {
 				
 			}
 			
-			//Execute filter plugins.
-			$merge_buffer = mergr_execute_plugins( "filter","css", $merge_buffer );
+			if (! $this->options['no-plugins'] ) {
+				//Execute filter plugins.
+				$merge_buffer = mergr_execute_plugins( "filter","css", $merge_buffer );
+			}
 			
 			//Write the file.
 			$fh = @fopen($directory . "/css/global.min.css", "w");
@@ -125,6 +129,39 @@ class mergr {
 	protected function plugin_execute($operation, $type) {
 		
 		global $mergr_plugins;
+		
+	}
+	
+	public function watch() {
+		
+		//Is this the first time we've run watch()?
+		if (! property_exists($this, "watchlist") ) {
+			//Hasn't been run yet.
+			$this->watchlist = null;
+			foreach($this->css_process as $file) {
+				$this->watchlist[] = array(
+					"file" => $file,
+					"hash" => hash_file("sha256", $file)
+				);
+			}
+			return true;
+		}
+		
+		$result = false;
+		
+		//Loop through each file in the watch list and calculate it's MD5 sum.
+		
+		for($i=0;$i<count($this->watchlist);$i++) {
+			$current_hash = hash_file("sha256", $this->watchlist[$i]["file"]);
+			if ( $current_hash != $this->watchlist[$i]["hash"] ) {
+				//Files have changed.
+				$result = true;
+				$this->watchlist[$i]["hash"] = $current_hash;
+			}
+		}
+		
+		
+		return $result;
 		
 	}
 	
--- mergr.php
+++ mergr.php
@@ -8,11 +8,38 @@ if ( $argc < 2 ) {
 	exit;
 }
 
-if ( $argc == 2 ) {
-	//First argument should be a directory.
-	$mergr_function = "once-off";
-	$mergr_directory = $argv[1];
+//Default function
+$mergr_function = "once-off";
+
+//Last argument should always be the directory.
+$mergr_directory = $argv[$argc-1];
+
+$mergr_options = array(
+	"no-plugins" => false,
+	"watch" => false
+);
+
+//Set command arguments.
+for($i=1;$i<($argc-1);$i++) {
+	if ( substr($argv[$i],0,2) == "--" ) {
+		//Check if there is a value.
+		$val = explode("=", $argv[$i]);
+		if ( count($val) > 1 ) {
+			$mergr_options[substr($val[0],2)] = $val[1];
+		} else {
+			$mergr_options[substr($argv[$i],2)] = true;
+		}
+	}
 }
+
+
+//Check options.
+//Set function accordingly.
+if ( $mergr_options['watch'] ) {
+	$mergr_function = "watch";
+	print "Mergr.php will watch " . $mergr_directory . " for changes\nPress CTRL + C to cancel\n";
+}
+
 
 //Browse for plugins.
 mergr_find_plugins();
@@ -26,8 +53,27 @@ switch($mergr_function) {
 		}
 		
 		//Do the merge.
-		$mergr = new mergr($mergr_directory);
+		$mergr = new mergr($mergr_directory, $mergr_options);
 		$mergr->merge_css($mergr_directory);
+		
+		break;
+		
+	case "watch":
+		
+		$mergr = new mergr($mergr_directory, $mergr_options);
+		
+		while (1) {
+			
+			if( $mergr->watch() ) {
+				//Update files.
+				print "Change detected. Re-merging.\n";
+				$mergr->merge_css($mergr_directory);
+			}			
+			
+			//Scan files every one minute.
+			sleep(1);
+			
+		}
 		
 		break;
 	
